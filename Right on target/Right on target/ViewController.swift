555a0c91218153e5eded5aeb8fd60b054340bb50
Oleksandr Sushchenko <a.suschenko@gmail.com>
2023-03-21 14:19:19 +0200

    MVC

--- Right on target/Right on target/ViewController.swift
+++ Right on target/Right on target/ViewController.swift
@@ -15,12 +15,13 @@ class ViewController: UIViewController {
     // Загадане число
     var number: Int = 0
     // Раунд
-    var round: Int = 0
-    // Сума очок за раунд
+    var round: Int = 1
+    // Сума очків за раунд
     var points: Int = 0
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        print("viewDidLoad")
         
         // Генеруємо випадкове число
         self.number = Int.random(in: 1...50)
@@ -29,46 +30,69 @@ class ViewController: UIViewController {
         
     }
     
+    override func loadView() {
+        super.loadView()
+        print("loadView")
+        
+        // Створюємо мітку для виведення номера версії
+        let versionLabel = UILabel(frame: CGRect(x: 20, y: 10, width: 200, height: 20))
+        // Текст мітки
+        versionLabel.text = "Версія 1.1"
+        // Додаємо мітку до батьківської view
+        self.view.addSubview(versionLabel)
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        print("viewWillAppear")
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        print("viewDidAppear")
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        print("viewWillDisappear")
+    }
+    
+    override func viewDidDisappear(_ animated: Bool) {
+        super.viewDidDisappear(animated)
+        print("viewDidDisappear")
+    }
+    
     @IBAction func checkNumber() {
         
-        // Якщо гра тільки починається
-        if self.round == 0 {
-            // Генеруємо випадкове число
-            self.number = Int.random(in: 1...50)
-            // Передаємо значення випадкового числа в label
-            self.label.text = String(self.number)
-            // Встановлюємо лічильник раундів на 1
-            self.round = 1
+        // Отримуємо значення на слайдері
+        let numSlider = Int(self.slider.value)
+        // Порівнюємо значення із загаданим та підраховуємо очки
+        if numSlider > self.number {
+            self.points += 50 - numSlider + self.number
+        } else if numSlider < self.number {
+            self.points += 50 - self.number + numSlider
         } else {
-            // Отримуємо значення на слайдері
-            let numSlider = Int(self.slider.value)
-            // Порівнюємо значення із загаданим та підраховуємо очки
-            if numSlider > self.number {
-                self.points += 50 - numSlider + self.number
-            } else if numSlider < self.number {
-                self.points += 50 - self.number + numSlider
-            } else {
-                self.points += 50
-            }
-            if self.round == 5 {
-                // АЛЕРТ виводимо інформаційне вікно з результатами гри
-                let alert = UIAlertController(
-                    title: "Гру закінчено",
-                    message: "Ви заробили \(self.points) очків",
-                    preferredStyle: .alert)
-                alert.addAction(UIAlertAction(title: "Почати заново", style:
-                        .default, handler: nil))
-                self.present(alert, animated: true, completion: nil)
-                self.round = 1
-                self.points = 0
-            } else {
-                self.round += 1
-            }
-            // Генеруємо випадкове число
-            self.number = Int.random(in: 1...50)
-            // Передаємо значення випадкового числа в label
-            self.label.text = String(self.number)
+            self.points += 50
         }
+        if self.round == 5 {
+            // АЛЕРТ виводимо інформаційне вікно з результатами гри
+            let alert = UIAlertController(
+                title: "Гру закінчено",
+                message: "Ви заробили \(self.points) очків",
+                preferredStyle: .alert)
+            alert.addAction(UIAlertAction(title: "Почати заново", style:
+                    .default, handler: nil))
+            self.present(alert, animated: true, completion: nil)
+            self.round = 1
+            self.points = 0
+        } else {
+            self.round += 1
+        }
+        // Генеруємо випадкове число
+        self.number = Int.random(in: 1...50)
+        // Передаємо значення випадкового числа в label
+        self.label.text = String(self.number)
+        
     }
     
 }
